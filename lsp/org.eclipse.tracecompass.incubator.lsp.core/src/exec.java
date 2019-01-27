e07b95c98aa6bae77c0c0954a076d191d61647ca
MaxTibs <maxx_thibault@hotmail.com>
2019-01-27 14:29:28 -0500

    Hello world
    Change-Id: Ie56eb3fd9ec87ad8713b9d6c8ffb14d9afc41376
    Signed-off-by: MaxTibs <maxx_thibault@hotmail.com>

--- lsp/org.eclipse.tracecompass.incubator.lsp.core/src/exec.java
+++ lsp/org.eclipse.tracecompass.incubator.lsp.core/src/exec.java
@@ -1,16 +1,16 @@
 
-import org.eclipse.tracecompass.incubator.internal.lsp.core.client.ClientImpl;
-import org.eclipse.tracecompass.incubator.internal.lsp.core.server.ServerImpl;
+import org.eclipse.tracecompass.incubator.internal.lsp.core.client.Client;
+import org.eclipse.tracecompass.incubator.internal.lsp.core.server.Server;
 
 public class exec {
 
     public static void main(String[] args) throws Exception {
 
-        //String host = "127.0.0.1";
-        //int port = 9090;
+        String host = "127.0.0.1";
+        int port = 9090;
 
-        new ServerImpl();
-        new ClientImpl();
+        new Server(host, port);
+        new Client(host, port);
 
     }
 }
